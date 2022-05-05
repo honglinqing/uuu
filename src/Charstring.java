03a06c703da79ef83584241ca5ec177c86e7463b
honglinqing <104840343+honglinqing@users.noreply.github.com>
2022-05-05 20:26:43 +0800

    Delete Charstring.java

--- src/Charstring.java
+++
@@ -1,9 +0,0 @@
-public class Charstring {
-    public static void main(String[] args) {
-     char[] arr={'孙','玉','冲','i'};
-     StringChar as=new StringChar();
-     String ss=as.changeChar(arr);
-        System.out.println(ss);
-    }
-
-}
